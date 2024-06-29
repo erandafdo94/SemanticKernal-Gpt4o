be72cf6b0a359ba63975aeff9dd7faa630b0234c
effdog <selfnba@gmail.com>
2024-06-29 18:04:00 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.SemanticKernel;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddKernel();
+builder.Services.AddOpenAIChatCompletion("gpt-4o", builder.Configuration["AI:OpenAI:ApiKey"]);
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+##This repo contains the basic implementation for semantic kernal with gpt4o
+Read this article for more inf.
+[Integrating .NET application with GPT-4o using Semantic Kernel](https://medium.com/@erandafernando/integrating-net-application-with-chat-gpt-4o-using-semantic-kernel-c7816ea3c339)
